bd8ee9a69195c1d9bbe503cf77f20ae602282e33
pedrobraga <pedro.braga@me.com>
2024-08-13 19:49:58 -0300

    some info from some of the staff I had from the top of my head

---
+++ kcl_staff.js
@@ -0,0 +1,19 @@
+db KCL_professors
+
+db.profiles.insertMany([
+    {name: "Wayne Dickson", university: "Kings College London", department: "Physics", field: "Nanooptics", interest_areas: ["nanophotonics", "photovoltaics", "quantum dots"], group_head: false},
+    {name: "Christopher Lorenz", university: "Kings College London", department: "Engineering", field: "Computational Biophysics", interest_areas: ["soft matter", "computational methods"], group_head: true},
+    {name: "Eugene Lim", university: "Kings College London", department: "Physics", field: "Astrophysics", interest_areas, group_head: },
+    {name: "Amelle Zair", university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: "Aleksandra Rakovich", university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: "Lev Kantorovitch", university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: "Francisco Fortuno", university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: "Katelyn Spillane", university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: "Jeffrey Grubbe", university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: , university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: , university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: , university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: , university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: , university: "Kings College London", department: , field: , interest_areas, group_head: },
+    {name: , university: "Kings College London", department: , field: , interest_areas, group_head: },
+])
